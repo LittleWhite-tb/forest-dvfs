ff57368e4556187decfd6f09b016c48e474cc2ed
jcbeyler <jcbeyler@bizet.(none)>
2011-06-27 18:09:27 +0200

    Fixed main:     - Added information
    Fixed makefile:
        - Added common
    
    Added Log system in Common
    
    Removed binary files

--- Version1/Main.c
+++ Version1/Main.c
@@ -16,67 +16,63 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
+#include <assert.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <ThreadedProfiler.h>
-
-#include <unistd.h>
 #include <string.h>
-#include <math.h>
+#include <unistd.h>
 
-#define MEM_BOUND_ITER (mem_iters)
+#include "Log.h"
+#include "ThreadedProfiler.h"
+
 #define MEM_BOUND_FOOTPRINT (4*1024*1024)
 
-double BigVec[MEM_BOUND_FOOTPRINT];
-long mem_iters=(11192);
-
-int
-main (void)
+/** 
+ * @brief Perform the benchmark 
+ */
+static void 
+benchmark (void)
 {
-	STPContext *handle;
-	SFuncsToUse profFuncs={decisionInit,decisionDestruct, decisionGiveReport};
-
+    //Dummy arrays
     double DummyVec1[12]={1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0};
     double DummyVec2[12]={1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0};
-    //a 1 gig vector definitely wont fit in cache
-
-    int expected_loop_interval=1000;
+ 
+    //Lots of random variables to make it all happy
     long updown_mem_product=5000;
     long cpu_2_mem_ratio=3793110;
-	
-	long long cpuBoundIter = 818822656;
-	long long memBoundIter = 9217728;
 
-	long long numUpDown = 25 ;
-	unsigned long long memBoundFootprint = 4*1024*1024;
-	
-	double BigVec[4*1024*1024];
-	expected_loop_interval = 1000;
-	
-	
+    //Set this to 1, it was: (60*1000)/(expected_loop_interval*2);
+    //No longer needed for the moment: int expected_loop_interval=1000;
+	long long numUpDown = 1;
+
+    unsigned long long memBoundFootprint = 4*1024*1024;
+	long long memBoundIter = round((float)updown_mem_product/(float)numUpDown);
+	long long cpuBoundIter = memBoundIter*cpu_2_mem_ratio;
+
+    //Our memory array
+	double *BigVec = NULL;
+    BigVec = malloc (MEM_BOUND_FOOTPRINT * sizeof (*BigVec));
+    assert (BigVec != NULL);
+
 	//calculate loop bounds
-	numUpDown = (60*1000)/(expected_loop_interval*2);
 	//once we have number of outer loops we use the product to find the memory loop bounds
-	memBoundIter = round((float)updown_mem_product/(float)numUpDown);
-	cpuBoundIter = memBoundIter*cpu_2_mem_ratio;
-
-	//now start!
-    printf ("REST Start\n");
-    handle=profilerInit(profFuncs);
 
 	int i;
 	for(i=0; i < numUpDown; i++)
 	{
 		//Now we do something cpu bound
-			
 		int j;	
 		double temp[4];
-		//force everything to stay in registers and unroll
+		
+        //force everything to stay in registers and unroll
 		temp[0]=DummyVec1[0];
 		temp[1]=DummyVec1[1];
 		temp[2]=DummyVec1[2];
 		temp[3]=DummyVec1[3];
-		for(j=0;j<cpuBoundIter;j++)
+		
+        //Do CPU
+        for(j=0;j<cpuBoundIter;j++)
 		{
 			temp[0]=temp[0]*temp[0];
 			temp[1]=temp[1]*temp[1];
@@ -84,22 +80,41 @@ main (void)
 			temp[3]=temp[3]*temp[3];
 			
 		}
+
 		DummyVec2[0]=temp[0];
 		DummyVec2[1]=temp[1];
 		DummyVec2[2]=temp[2];
 		DummyVec2[3]=temp[3];
 
-
+        //Do Memory
 		for(j=0;j<memBoundIter;j++)
 		{
 			memcpy(&BigVec[0],&BigVec[memBoundFootprint/2],memBoundFootprint/2*sizeof(*BigVec));
 		}
-		
 	}
-    
 
-    profilerDestroy(handle);
+    //Delete array
+    free (BigVec), BigVec = NULL;
+}
 
+int
+main (void)
+{
+	STPContext *handle;
+	SFuncsToUse profFuncs={decisionInit,decisionDestruct, decisionGiveReport};
+
+    //Set logger
+    Log_setOutput (stderr);
+	
+	//now start!
+    Log_output (0, "REST Start\n");
+    handle = profilerInit (profFuncs);
+
+    benchmark ();
+
+    profilerDestroy (handle), handle = NULL;
+
+    Log_output (0, "REST Stop\n");
  
     return EXIT_SUCCESS;
 }
