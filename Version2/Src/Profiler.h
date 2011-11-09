319843c583b6c2faf9966d7dc695d17bf103c93b
Thibault Fighiera <thibault.fighiera@exascale-computing.eu>
2011-11-09 16:40:10 +0100

    Starting new cpp version of rest

---
+++ Version2/Src/Profiler.h
@@ -0,0 +1,45 @@
+/*
+   Copyright (C) 2011 Exascale Research Center
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+/**
+  @file Profiler.h
+  @brief The Profiler class header is in this file 
+ */
+#ifndef H_PROFILER
+#define H_PROFILER
+
+/**
+ * @class Profiler
+ * @brief The Profiler of ThreadedProfiler and ProfilerServer 
+ */
+class Profiler
+{
+	
+	public:
+		/**
+		 * @brief Constructor
+		 */
+		Profiler (void);
+		
+		/**
+		 * @brief Destructor
+		 */
+		virtual ~Profiler (void);
+};
+
+#endif
