a7fdd402f4a8a1daed21b25f03f163419ef3c001
Alexandre Laurent <alexandre.laurent@exascale-computing.eu>
2013-02-27 10:27:28 +0100

    Remove duplicate code for unit creation

--- LocalRest/Src/LocalRest.cpp
+++ LocalRest/Src/LocalRest.cpp
@@ -115,7 +115,7 @@ int main (int argc, char ** argv)
 	handleAllocation (restCtx.thdCtx);
 	
 	// launch threads
-	for (unsigned int i = 1; i < nbDVFSUnits; i++)
+	for (unsigned int i = 0; i < nbDVFSUnits; i++)
 	{
 		DVFSUnit& unit = restCtx.cnfo->getDVFSUnit (i);
 		// initialize options	
@@ -127,23 +127,17 @@ int main (int argc, char ** argv)
 		handleAllocation (opts.dec);
 		opts.unit = &unit;
 
-		// run thread
-		if (pthread_create (&restCtx.thdCtx [i].pid, NULL, thProf, &opts) != 0)
-		{
-			std::cerr << "Error: Failed to create profiler thread" << std::endl;
-			return EXIT_FAILURE;
-		}
+		// The unit 0 will use the current application thread. Others need
+      // a thread to run
+      if ( i != 0 )
+      {
+         if (pthread_create (&restCtx.thdCtx [i].pid, NULL, thProf, &opts) != 0)
+         {
+            std::cerr << "Error: Failed to create profiler thread" << std::endl;
+            return EXIT_FAILURE;
+         }
+      }
 	}
-
-	// the main process is the main profiler
-	DVFSUnit& unit0 = restCtx.cnfo->getDVFSUnit (0);
-	thOpts& opts = restCtx.thdCtx [0].opts;
-	opts.id = 0;
-	opts.dec = new NewAdaptiveDecisions (unit0, mode);
-	handleAllocation (opts.dec);
-	opts.prof = new PfmProfiler (unit0);
-	handleAllocation (opts.dec);
-	opts.unit = &unit0;
 
 	// cleanup stuff when exiting
 #ifdef REST_LOG
@@ -153,7 +147,7 @@ int main (int argc, char ** argv)
 	atexit (exitCleanup);
 
 	// Launch the function for main thread
-	thProf (&opts);
+	thProf (&restCtx.thdCtx[0].opts);
 
 	// never reached
 	return EXIT_SUCCESS;
